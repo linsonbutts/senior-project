b42a9c331facbdc6d6d7fea393eba95fd6300272
unknown <linson.butts@students.cau.edu>
2021-03-22 00:45:54 -0400

    *sigh* still cannot get bluetooth communication*

--- src/guide-app/.eslintrc.js
+++ src/guide-app/.eslintrc.js
@@ -20,13 +20,5 @@ module.exports = {
     ],
     "rules": {
         "react/prop-types": [2, { ignore: ['children','trigger'] },]
-    },
-    'rules': {
-        // we want to force semicolons
-        'semi': ['warning', 'always'],
-        // we use 2 spaces to indent our code
-        'indent': ['warning', 1],
-        // we want to avoid useless spaces
-        'no-multi-spaces': ['warning']
+    }
 }
-};

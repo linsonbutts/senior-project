4168b3c84ea5a096d63c9c1f3271cb51bc5d5ae9
unknown <linson.butts@students.cau.edu>
2021-04-06 22:16:11 -0400

    Added Pie chart

--- src/guide-app/src/components/Building.js
+++ src/guide-app/src/components/Building.js
@@ -1,12 +1,12 @@
 import Popup from './Popup'
 import styled from 'styled-components'
-import React,{useState, useEffect,useRef} from 'react'
+import React,{useState,useRef} from 'react'
 import Woody from '../assets/library.jpg'
 import Arnett from '../assets/arnett.jpg'
 import Layout from './Layout'
 import InterestPoint from './InterestPoint'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
-import {BarChart,Tooltip,Bar, Legend, CartesianGrid, XAxis, YAxis} from 'recharts';
+import {BarChart,Tooltip,Bar, Legend, CartesianGrid, XAxis, YAxis, PieChart, Pie} from 'recharts';
 import 'react-tabs/style/react-tabs.css';
 
 let BuildingPic = styled.img`
@@ -56,21 +56,33 @@ let [tabIndex, setTabIndex] = useState(0);
 let [click,setClick] = useState(false)
 let [nearest, setNearest] = useState(Woody)
 let [nearestText,setNearestText] = useState(buildingText[0])
+let arnTime = useRef(0)
+let woodTime = useRef(0)
 const iotDevice = useRef('')
 let iotTemp = useRef('')
 
 //Used to track the total session and building time.
+//Will update the time data for pie and bar chart
 let connectionTime = useRef(0)
-let arnTime = useRef(0)
-let woodTime = useRef(0)
+let arnSec = useRef(0)
+let woodSec = useRef(0)
 let arnRunning = false
 let woodRunning = false
+let neitherPercent = useRef(0);
+let arnPercent = useRef(0);
+let woodPercent = useRef(0);
 
 //Used for building the charts
 const barData = [
-    {name: "Arnett Time", atime: arnTime.current},
-    {name: "Woody Time", wtime:  woodTime.current}
+    {name: "Arnett Time", Time_in_Arnett: arnTime.current},
+    {name: "Woody Time", Time_in_Woodruff:  woodTime.current}
 ]
+const pieData = [
+    {name: "% of time in Neither", Time_Percentage: neitherPercent.current},
+    {name: "% of time in Arnett", Time_Percentage: arnPercent.current},
+    {name: "% of time in Woodruff", Time_Percentage: woodPercent.current}
+]
+
 const tempHandler = async (event) => {
     iotTemp.current = event.detail
     console.log(iotTemp.current)
@@ -82,8 +94,8 @@ const tempHandler = async (event) => {
 
 const buttonA_Handler = (event) => {
     if(event.detail == 1){
-        arnRunning = true;
-        woodRunning = false;
+        arnRunning = true
+        woodRunning = false
     }
     if(event.detail == 2){
         arnRunning = false
@@ -92,45 +104,57 @@ const buttonA_Handler = (event) => {
 const buttonB_Handler = (event) => {
     if(event.detail == 1){
         woodRunning = true
-        arnRunning = false;
+        arnRunning = false
     }
     else if(event.detail == 2){
         woodRunning = false
     }
 }
 
-function addWoodyTime(){
+function addWoodySec(){
     if(woodRunning == false){
+        neitherPercent.current = (connectionTime.current-woodSec.current+arnTime.current)/connectionTime.current*100
         return
     }
     else{
-        woodTime.current +=1
-        console.log("Time spent in WOOD: "+woodTime.current)
+        //Create the percentages for the pie chart and the time in minutes for bar chart
+        woodSec.current +=1
+        woodPercent.current = (woodSec.current/connectionTime.current)*100
+        console.log("Time spent in WOOD: "+woodSec.current)
+        if(woodSec.current % 60 === 0){
+            woodTime.current += 1
+        }
     };
 }
-function addArnTime(){
+function addArnSec(){
     if(arnRunning == false){
+        neitherPercent.current = (connectionTime.current-woodSec.current+arnTime.current)/connectionTime.current*100
         return
     }
     else{
-        arnTime.current +=1
-        console.log("Time spent in ARN: "+arnTime.current)
+        arnSec.current +=1
+        arnPercent.current = arnSec.current/connectionTime.current*100
+        console.log("Time spent in ARN: "+arnSec.current)
+        if(arnSec.current % 60 === 0){
+            arnTime.current += 1
+        }
     };
 }
 
-var arnInterval = setInterval(addArnTime,1000)
-var woodInterval = setInterval(addWoodyTime,1000)
+var arnInterval = setInterval(addArnSec,1000)
+var woodInterval = setInterval(addWoodySec,1000)
 //Used to connect to microBit
 let BTcheck = async () => {
     //Used to read how long a user spends connected to microbit
     setInterval(function(){
-        connectionTime +=1
+        connectionTime.current +=1
     },1000);
     console.log('The button was pressed')
     try{
         let device = await microBT.requestMicrobit(window.navigator.bluetooth);
         await device.gatt.connect()
         await console.log(device.gatt.connected)
+        let services = await microBT.getServices(device)
         iotDevice.current = await microBT.getServices(device)
         console.log(iotDevice.current)
         //Services are then logged
@@ -163,18 +187,22 @@ let handleNearest = async () => {
 
 let handleClick = () => setClick(!click)
 
-useEffect (()=>{
-handleNearest()
-},[iotTemp])
     return(
      
         <React.Fragment>
             <script type="text/javascript" src="microbit.umd.js"></script>
             <Layout>
-            <MicroButton onClick ={BTcheck}>
+                <h3>
+                    Click here to find MicroBit
+                </h3>
+                <MicroButton onClick ={BTcheck}>
                     Microbit Connect
                 </MicroButton>
-
+                <p>
+                    Hit A or B on microBit to indicate you have entered a building. <br></br>
+                    You will be asked if you have entered a building <br></br>
+                    if the environment around you has changed
+                </p>
                 <div>
                 <Tabs selectedIndex={tabIndex} onSelect={index => setTabIndex(index)}>
                     <TabList>
@@ -200,9 +228,14 @@ handleNearest()
                         <YAxis />
                         <Tooltip />
                         <Legend />
-                        <Bar dataKey="atime" fill="#8884d8" />
-                        <Bar dataKey="wtime" fill="#82ca9d" />
+                        <Bar dataKey="Time_in_Arnett" fill="#8884d8" />
+                        <Bar dataKey="Time_in_Woodruff" fill="#82ca9d" />
                     </BarChart>
+
+                    <PieChart width={730} height={250}>
+                        <Pie data={pieData} dataKey="Time_Percentage" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" label={(entry) => entry.name}/>
+                    </PieChart>
+                    
                     </TabPanel>
                     </Tabs>
                     </div>
